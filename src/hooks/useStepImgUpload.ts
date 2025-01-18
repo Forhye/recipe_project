817b5afd1916c5a33ad201216fde888203c3e932
Forhye <onlyh4425@gmail.com>
2025-01-18 15:46:18 +0900

    ♻️ feat : 이미지 업로드 최적화 - 이미지 컴프레션을 사용해서 클라이언트가 이미지 업로드 시 자동으로 압축하는 기능 추가 - browser-image-compresstion 라이브러리 추가 - 최대 파일 크기 사이즈 0.5MB - 썸네일 최대 크기 960*960 - 레시피 스텝 최대 크기 152*152

--- src/hooks/useStepImgUpload.ts
+++ src/hooks/useStepImgUpload.ts
@@ -6,6 +6,7 @@ import {
 	getDownloadURL,
 } from 'firebase/storage';
 import { v4 as uuidv4 } from 'uuid';
+import imageCompression from 'browser-image-compression';
 
 const useStepImgUpload = () => {
 	const storage = getStorage();
@@ -13,31 +14,43 @@ const useStepImgUpload = () => {
 		[key: number]: number;
 	}>({}); // 단계별 진행률 상태 추가
 
-	const uploadStepImage = (file: File, index: number) => {
-		return new Promise<string>((resolve, reject) => {
-			const uniqueFileName = `${uuidv4()}_${file.name}`;
-			const storageRef = ref(storage, `images/step/${uniqueFileName}`);
-			const uploadTask = uploadBytesResumable(storageRef, file);
+	const options = {
+		maxSizeMB: 0.5,
+		maxWidthOrHeight: 152,
+	};
 
-			uploadTask.on(
-				'state_changed',
-				(snapshot) => {
-					const progress =
-						(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-					setUploadProgress((prev) => ({ ...prev, [index]: progress })); // 단계별 진행률 업데이트
-				},
-				(error) => {
-					console.error('Error uploading image:', error);
-					reject(error);
-				},
-				() => {
-					getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-						setUploadProgress((prev) => ({ ...prev, [index]: 0 })); // 업로드 완료 후 진행률 초기화
-						resolve(downloadURL);
-					});
-				}
-			);
-		});
+	const uploadStepImage = async (file: File, index: number) => {
+		try {
+			const compressedFile = await imageCompression(file, options);
+
+			const uniqueFileName = `${uuidv4()}_${compressedFile.name}`;
+			const storageRef = ref(storage, `images/step/${uniqueFileName}`);
+			const uploadTask = uploadBytesResumable(storageRef, compressedFile);
+
+			return new Promise<string>((resolve, reject) => {
+				uploadTask.on(
+					'state_changed',
+					(snapshot) => {
+						const progress =
+							(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+						setUploadProgress((prev) => ({ ...prev, [index]: progress })); // 단계별 진행률 업데이트
+					},
+					(error) => {
+						console.error('Error uploading image:', error);
+						reject(error);
+					},
+					() => {
+						getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+							setUploadProgress((prev) => ({ ...prev, [index]: 0 })); // 업로드 완료 후 진행률 초기화
+							resolve(downloadURL);
+						});
+					}
+				);
+			});
+		} catch (error) {
+			console.error('Error compressing image:', error);
+			return Promise.reject(error);
+		}
 	};
 
 	return {
--- src/hooks/useThumbnailImgUpload.ts
+++ src/hooks/useThumbnailImgUpload.ts
@@ -2,15 +2,26 @@ import { useState } from 'react';
 import { storage } from '../firebase/config'; // Firebase 설정 파일 import
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { v4 as uuidv4 } from 'uuid'; // UUID import
+import imageCompression from 'browser-image-compression';
 
 const useThumbnailImgUpload = () => {
 	const [imageUrl, setImageUrl] = useState<string | null>(null);
 
 	const uploadImage = async (file: File) => {
+		console.log('업로드 파일', file);
+		const options = {
+			maxSizeMB: 0.5,
+			maxWidth: 960,
+			maxHeight: 960,
+		};
 		const uniqueFileName = `${uuidv4()}_${file.name}`; // UUID와 원래 파일명을 조합
 		const storageRef = ref(storage, `images/thumbnail/${uniqueFileName}`); // 고유한 파일명으로 경로 설정
+
 		try {
-			await uploadBytes(storageRef, file); // 이미지 업로드
+			const compressedFile = await imageCompression(file, options);
+
+			await uploadBytes(storageRef, compressedFile); // 이미지 업로드
+
 			const url = await getDownloadURL(storageRef); // 업로드된 파일의 다운로드 URL 가져오기
 			setImageUrl(url); // URL 상태 업데이트
 		} catch (error) {
