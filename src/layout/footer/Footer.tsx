4c0d6f4c4b7b548dac789ffe5dfaa4d0d599fd9f
Forhye <onlyh4425@gmail.com>
2025-01-25 13:33:57 +0900

    ♻️ feat : 푸터 로그인/로그아웃/회원가입 버튼 수정 - 현재 유저 상태에 따라 로그인/로그아웃, 회원가입 버튼의 유무를 수정

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,9 +1,12 @@
+import { useAuth } from '../../context/AuthContext';
 import styles from './Footer.module.css';
 import githubImg from '/assets/icon_github.svg';
 import logoGrayImg from '/assets/logo_gray.svg';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+	const { user, logout } = useAuth(); // 현재 유저와 로그아웃 함수 가져오기
+
 	return (
 		<footer id={styles.footer}>
 			<section className={styles.footerSection}>
@@ -12,12 +15,21 @@ const Footer = () => {
 						<li className={styles.logoGrayImg}>
 							<img src={logoGrayImg} alt="회색 레시피 연구소 로고" />
 						</li>
-						<li className={styles.footerMenuText}>
-							<Link to="/login">로그인</Link>
-						</li>
-						<li className={styles.footerMenuText}>
-							<Link to="/signup">회원가입</Link>
-						</li>
+						{user ? (
+							<li className={styles.footerLogOutMenu} onClick={logout}>
+								로그아웃
+							</li>
+						) : (
+							<ul className={styles.footerLoginMenu}>
+								<li className={styles.footerMenuText}>
+									<Link to="/login">로그인</Link>
+								</li>
+								<li className={styles.footerMenuText}>
+									<Link to="/signup">회원가입</Link>
+								</li>
+							</ul>
+						)}
+
 						<li className={styles.footerMenuText}>
 							<Link to="/create">레시피 작성</Link>
 						</li>
